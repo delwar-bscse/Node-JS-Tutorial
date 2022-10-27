bfea7f7c42d4b7af4c9fa60b7d26073135bae16b
M D Hossain <delwarbscse@gmail.com>
2022-10-27 23:14:52 +0600

    Learn use of 'unlink'

--- Lesson-02/index.js
+++ Lesson-02/index.js
@@ -1,12 +1,12 @@
 
 const fs = require('fs');
 
-// fs.rename('previousFileName','newFileName, (err)=>{})
+// fs.rename('fileName', (err)=>{})
 
-fs.rename('Demo1.txt','Demo2.txt', (err)=>{
+fs.unlink('Demo2.txt', (err)=>{
     if(err){
         console.log(err);
     }else{
-        console.log(`Successfully Renamed`);
+        console.log(`Successfully Deleted`);
     }
 })
