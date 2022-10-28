b63451497559880b3960e759b091a0670955fbfa
M D Hossain <delwarbscse@gmail.com>
2022-10-29 00:25:01 +0600

    external npm use

---
+++ Lesson-05/server.js
@@ -0,0 +1,6 @@
+
+const randomFruitsName = require('random-fruits-name');
+const randomMoviesName = require('movies-names');
+
+console.log(randomFruitsName('en'));
+console.log(randomMoviesName.random());
