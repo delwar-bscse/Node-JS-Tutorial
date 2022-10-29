d71620dc073ec4d8fb22876ff85d71f2afe11496
M D Hossain <delwarbscse@gmail.com>
2022-10-29 12:36:42 +0600

    http routing & nodemon package 2

--- Lesson-06/index.js
+++ Lesson-06/index.js
@@ -1,12 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
 const hostname = '127.0.0.1';
 
 const myServer = http.createServer((req,res)=>{
-    console.log(req.url);
-    res.write("nodemon work perfectly.")
-    res.end();
+    const handleFile = (statusCode, fileName) =>{
+        fs.readFile(fileName,(err,data)=>{
+            res.writeHead(statusCode, {'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        });
+    }
+    if(req.url==='/'){
+        handleFile(200, "./myViews/index.html");
+    }
+    else if(req.url==='/about'){
+        handleFile(200, "./myViews/about.html");
+    }
+    else if(req.url==='/contact'){
+        handleFile(200, "./myViews/contact.html");
+    }
+    else{
+        handleFile(404, "./myViews/error.html");
+    }
 })
 
 myServer.listen(port, hostname, ()=>{
